fde47177fc202167849daf9d13493e8985ea08c1
Panu Kettunen <panu.t.kettunen@gmail.com>
2020-08-22 01:26:43 +0300

    Use parsefloat instead of parseint for checkint wallet amount

--- src/components/Pool/Pool.tsx
+++ src/components/Pool/Pool.tsx
@@ -200,7 +200,7 @@ function Pool({ pool }: Props) {
             })}
           </div>
         )}
-        {parseInt(maxAmount) > 0 && hasAllowance ? (
+        {parseFloat(maxAmount) > 0 && hasAllowance ? (
           <>
             <div className="stake">
               {hasAllowance && (
