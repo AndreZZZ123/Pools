47dbe56558a1f12be456ee490544056e86463113
andrezzz123 <adrezzz123@gmail.com>
2020-08-22 16:08:50 +0800

    Fixed staking bug due to rounding

--- src/helpers/eth.tsx
+++ src/helpers/eth.tsx
@@ -11,7 +11,12 @@ export async function getERC20balance(
 ) {
   const contract = new ethers.Contract(token.address, token.abi, provider);
   const amount = await contract.balanceOf(address);
-  return parseFloat(formatEther(amount)).toFixed(4);
+  // if(token.address == "0xc75F15AdA581219c95485c578E124df3985e4CE0") {
+  //   console.log(parseFloat(formatEther(amount)).toFixed(20))
+  //   console.log(parseFloat(formatEther(amount)))
+  //   console.log('---')
+  // }
+  return parseFloat(formatEther(amount)).toFixed(18);
 }
 
 export async function getCurrentTotalStake(pool: Pool, provider) {
